1c1114bc4e3b411372e13121572e1c8e660b1cef
mrbillgates2001 <shahboznabiyev2001@gmail.com>
2023-10-25 14:30:36 +0500

    home and courses accomplished

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var burgerMenu = document.getElementById('burger-menu');
+
+var overlay = document.getElementById('menu');
+
+burgerMenu.addEventListener('click', function() {
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+});
+
+let modeBtn = document.getElementById("moon");
+
+modeBtn.addEventListener("click", function () {
+  document.body.classList.toggle("dark"); 
+});
+
+//* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
